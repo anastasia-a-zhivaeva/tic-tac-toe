6ead77d75d47d7ad118e9f1e17853825a2360770
anastasia-a-zhivaeva <anastasia.a.zhivaeva@gmail.com>
2021-02-22 19:43:14 +0300

    Added CircleCI config

--- src/components/Square/__tests__/Square-test.tsx
+++ src/components/Square/__tests__/Square-test.tsx
@@ -1,19 +1,16 @@
 import React from "react";
-import ReactDOM from 'react-dom';
-import { unmountComponentAtNode } from 'react-dom';
+import { render, unmountComponentAtNode } from 'react-dom';
 import { act } from 'react-dom/test-utils';
 
 import Square from '../Square';
 
 let container: HTMLDivElement | null = null;
 beforeEach(() => {
-  // setup a DOM element as a render target
   container = document.createElement("div");
   document.body.appendChild(container);
 });
 
 afterEach(() => {
-  // cleanup on exiting
   unmountComponentAtNode(container!);
   container?.remove();
   container = null;
@@ -21,7 +18,7 @@ afterEach(() => {
 
 it("renders with value = 0, isWinnerMove = false => textContent = 0, className = square", () => {
   act(() => {
-    ReactDOM.render(<Square value={'0'} isWinnerMove={false} onClick={() => {}} />, container);
+    render(<Square value={'0'} isWinnerMove={false} onClick={() => {}} />, container);
   });
   const button: HTMLButtonElement = (container as HTMLDivElement).firstChild as HTMLButtonElement;
   expect(button).toBeDefined();
@@ -31,7 +28,7 @@ it("renders with value = 0, isWinnerMove = false => textContent = 0, className =
 
 it("renders with value = 1, isWinnerMove = true => textContent = 1, className = winner-square", () => {
   act(() => {
-    ReactDOM.render(<Square value={'1'} isWinnerMove={true} onClick={() => {}} />, container);
+    render(<Square value={'1'} isWinnerMove={true} onClick={() => {}} />, container);
   });
   const button: HTMLButtonElement = (container as HTMLDivElement).firstChild as HTMLButtonElement;
   expect(button).toBeDefined();
@@ -42,7 +39,7 @@ it("renders with value = 1, isWinnerMove = true => textContent = 1, className = 
 it("calls event when clicked", () => {
   const onClick = jest.fn();
   act(() => {
-    ReactDOM.render(<Square value={'0'} isWinnerMove={false} onClick={onClick} />, container);
+    render(<Square value={'0'} isWinnerMove={false} onClick={onClick} />, container);
   });
   const button: HTMLButtonElement = (container as HTMLDivElement).firstChild as HTMLButtonElement;
 
